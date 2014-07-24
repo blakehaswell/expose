494a1f581f7db25aed1aabc026013af21b6a1c5c
Blake Haswell <haswell00@gmail.com>
2014-07-24 13:53:23 +1000

    Added ability to specify search dir.

--- src/expose.hs
+++ src/expose.hs
@@ -1,18 +1,24 @@
 import Control.Monad (filterM)
+import Data.List (elemIndex)
 import System.Directory (doesDirectoryExist, doesFileExist,
-                         getCurrentDirectory, getDirectoryContents)
+                         getDirectoryContents)
 import System.Environment (getArgs)
-import System.FilePath ((</>), splitDirectories)
+import System.FilePath ((</>), makeRelative, splitDirectories)
 
 main :: IO ()
 main = do
     args <- getArgs
-    currentDir <- getCurrentDirectory
-    dirContents <- getDirectoryContents currentDir
-    let isRecursive = "-r" `elem` args
-    files <- if isRecursive then recursiveGetFiles "" dirContents
-                            else getFiles $ map ("" </>) dirContents
-    putStrLn . unlines $ files
+    let searchDir = getSearchDir args
+        isRecursive = "-r" `elem` args
+    dirContents <- getDirectoryContents searchDir
+    files <- if isRecursive then recursiveGetFiles searchDir dirContents
+                            else getFiles $ map (searchDir </>) dirContents
+    putStrLn . unlines . map (makeRelative searchDir) $ files
+
+getSearchDir :: [String] -> FilePath
+getSearchDir args = case elemIndex "-d" args of
+                        Just i -> args !! (i + 1)
+                        Nothing -> "."
 
 getFiles :: [FilePath] -> IO [FilePath]
 getFiles = filterM doesFileExist
@@ -21,8 +27,8 @@ getDirs :: [FilePath] -> IO [FilePath]
 getDirs = filterM isDir
     where
         isDir dir = if lastDir dir `elem` [".", ".."]
-                            then return False
-                            else doesDirectoryExist dir
+                      then return False
+                      else doesDirectoryExist dir
         lastDir = last . splitDirectories
 
 recursiveGetFiles :: FilePath -> [FilePath] -> IO [FilePath]
